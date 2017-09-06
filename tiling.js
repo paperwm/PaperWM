106644a8479c474d7c7a645add610e11b1eb0979
Tor Hedin Brønner <torhedinbronner@gmail.com>
2017-09-06 21:37:47 +0200

    Some extra stuff

--- tiling.js
+++ tiling.js
@@ -1,8 +1,16 @@
+// Some notes:
+/* Signals:
+   "window-added" (workspace)
+   "window-removed" (workspace)
+
+ */
+
 
 glib = imports.gi.GLib
 
 Tweener = imports.ui.tweener;
-move = (actor, x, y) => {
+move = (meta_window, x, y) => {
+    let actor = meta_window.get_compositor_private()
     let buffer = actor.meta_window.get_buffer_rect();
     let frame = actor.meta_window.get_frame_rect();
     let x_offset = frame.x - buffer.x;
@@ -16,13 +24,6 @@ move = (actor, x, y) => {
 
 }
 
-move(meta_window.get_compositor_private(), 400, 20)
-//: t
-move(pages[3].get_compositor_private(), 700, 500)
-//: t
-
-meta_window.get_frame_rect().x
-
 timestamp = () => {
     return glib.get_monotonic_time()/1000
 }
@@ -35,35 +36,42 @@ rect = (meta_window) => {
     frame = meta_window.get_frame_rect()
     return [frame.x, frame.x + frame.width]
 }
-
 ensure_viewport = (meta_window) => {
     let [start, end] = rect(meta_window)
 
+    let index = pages.indexOf(meta_window)
     if (end > global.screen_width) {
-        meta_window.move_frame(true, start - (end - global.screen_width) - 10
-                               , meta_window.get_frame_rect().y)
-
-
+        let delta = start - (end - global.screen_width) - 10
+        propogate_backward(index, start - (end - global.screen_width) - 10)
+        propogate_forward(index + 1, global.screen_width)
     }
     else if (start < 0) {
-        meta_window.move_frame(true, 10
-                               , meta_window.get_frame_rect().y)
+        propogate_forward(pages.indexOf(meta_window), 10)
+        propogate_backward(index - 1, -global.screen_width)
     }
 }
 
-ensure_viewport(pages[0])
-//: t
-
 focus_handler = (meta_window, user_data) => {
-    let last = pages[focus]
 
-    [start, end] = rect(meta_window)
-
-    if (end > global.screen_width) {
-        meta_window.
-    }
+    ensure_viewport(meta_window)
+    meta_window.activate(timestamp())
 
     focus = pages.indexOf(meta_window)
+}
+
+propogate_forward = (n, x) => {
+    if (n < 0 || n >= pages.length)
+        return
+    let meta_window = pages[n]
+    move(meta_window, x, meta_window.get_frame_rect().y)
+    propogate_forward(n+1, x+meta_window.get_frame_rect().width)
+}
+propogate_backward = (n, x) => {
+    if (n < 0 || n >= pages.length)
+        return
+    let meta_window = pages[n]
+    move(meta_window, x, meta_window.get_frame_rect().y)
+    propogate_forward(n-1, x-meta_window.get_frame_rect().width )
 }
 
 focus_wrapper = (meta_window, user_data) => {
@@ -71,52 +79,63 @@ focus_wrapper = (meta_window, user_data) => {
 }
 
 // Run on new windows
-global.display.connect("window-created", (display, meta_window) => {
+// global.display.connect("window-created", (display, meta_window) => {
+//     pages.splice(focus + 1, 0, meta_window)
+//     meta_window.connect("focus", focus_wrapper)
+// })
+
+workspace = global.screen.get_active_workspace()
+workspace.connect("window-added", (ws, meta_window) => {
     pages.splice(focus + 1, 0, meta_window)
     meta_window.connect("focus", focus_wrapper)
 })
 
-pages[1].move_frame(true, 0, 0)
-
-let length = 0
-pages.map((meta_window) => {
-    let width = meta_window.get_frame_rect().width
-    meta_window.move_resize_frame(true, length, 0, width, global.screen_height)
-    length += width + 10
+workspace.connect("window-removed", (ws, meta_window) => {
+    pages.splice(pages.indexOf(meta_window), 1)
 })
 
 
+// pages[1].move_frame(true, 0, 0)
 
-web = global.window_group.get_children()[3].meta_window
-
-web.get_frame_rect().height
-//: 707
-
-web.move_resize_frame(true, -500, 20, web.get_frame_rect().width, web.get_frame_rect().height)
-
-// Example to move a frame
-win = global.window_group.get_children()[2]
-metawin = win.get_meta_window()
-metawin.move_resize_frame(false, global.screen_width + 20,30, 400, 700)
-
-//: t
-
-
-global.screen_width
-//: 1346
+// let length = 0
+// pages.map((meta_window) => {
+//     let width = meta_window.get_frame_rect().width
+//     meta_window.move_resize_frame(true, length, 0, width, global.screen_height)
+//     length += width + 10
+// })
 
 
 
-nth_window
-ensure_in_viewport()
-goto_next
-goto_prev
+// // web = global.window_group.get_children()[3].meta_window
 
-add_at_nth
-destroy
+// web.get_frame_rect().height
+// //: 707
+
+// web.move_resize_frame(true, -500, 20, web.get_frame_rect().width, web.get_frame_rect().height)
+
+// // Example to move a frame
+// win = global.window_group.get_children()[2]
+// metawin = win.get_meta_window()
+// metawin.move_resize_frame(false, global.screen_width + 20,30, 400, 700)
+
+// //: t
 
 
-move(meta_window, x)
-resize
+// global.screen_width
+// //: 1346
+
+
+
+// nth_window
+// ensure_in_viewport()
+// goto_next
+// goto_prev
+
+// add_at_nth
+// destroy
+
+
+// move(meta_window, x)
+// resize
 
 
