068a2e75b3e004acb124148519687a300c8c0532
Tor Hedin Brønner <torhedinbronner@gmail.com>
2017-09-10 17:58:40 +0200

    Add close window binding

--- tiling.js
+++ tiling.js
@@ -241,5 +241,6 @@ settings = new Gio.Settings({ schema_id: "org.gnome.desktop.wm.keybindings"});
 settings.set_strv("cycle-windows", ["<alt>period"])
 settings.set_strv("cycle-windows-backward", ["<alt>comma"])
 settings.set_strv("switch-windows", [])
+settings.set_strv("close", ['<super>c'])
 Meta.keybindings_set_custom_handler("cycle-windows", next);
 Meta.keybindings_set_custom_handler("cycle-windows-backward", previous);
