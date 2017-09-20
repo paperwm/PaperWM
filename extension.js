ab1c99da0100498e3fe9e0d48330c6b545ac0b5e
Tor Hedin Brønner <torhedinbronner@gmail.com>
2017-09-20 14:52:44 +0200

    Add bare bones necessary files for extension packing

---
+++ extension.js
@@ -0,0 +1,12 @@
+const Extension = imports.misc.extensionUtils.getCurrentExtension();
+const Tiling = Extension.imports.tiling;
+
+
+function init() {
+}
+
+function enable() {
+}
+
+function disable() {
+}
