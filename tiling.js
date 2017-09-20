769891f9b2760a7827886de5673a56d4615b089c
Tor Hedin Brønner <torhedinbronner@gmail.com>
2017-09-20 13:32:27 +0200

    Move _repl to own file

--- tiling.js
+++ tiling.js
@@ -18,44 +18,10 @@ global.stage.get_first_child().get_children().forEach((actor) => {
     }
 })
 statusbar_height = statusbar.height
-margin_tb = 2
-
-stack_margin = 75
 
 workspaces = []
 for (let i=0; i < global.screen.n_workspaces; i++) {
     workspaces[i] = []
-}
-
-function _repl() {
-    add_all_from_workspace()
-
-    meta_window = global.display.focus_window;
-    workspace = meta_window.get_workspace();
-    window_actor = meta_window.get_compositor_private();
-
-    set_action_handler("toggle-scratch-layer", () => { print("It works!"); });
-
-    meta_window = pages[0]
-//: [object instance proxy GType:MetaWindowX11 jsobj@0x7f8c39e52f70 native@0x3d43880]
-    workspace = meta_window.get_workspace()
-//: [object instance proxy GIName:Meta.Workspace jsobj@0x7f8c47166790 native@0x23b5360]
-
-    actor = meta_window.get_compositor_private()
-
-    St = imports.gi.St;
-    St.set_slow_down_factor(1);
-    St.set_slow_down_factor(3);
-
-    actor.z_position
-
-    meta = imports.gi.Meta
-    meta_window.get_layer()
-
-    // Use to control the stack level
-    meta_window.raise()
-    meta_window.lower()
-
 }
 
 debug_all = true; // Consider the default value in `debug_filter` to be true
