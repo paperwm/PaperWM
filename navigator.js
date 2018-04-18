b48ac2fd1532187b96c8c8e464c0d9f13f2a0311
Tor Hedin Brønner <torhedinbronner@gmail.com>
2018-04-19 00:41:28 +0200

    _previewWorkspace ⟶ switchWorkspace
    Move _previewWorkspace off of WindowManager, and rename to a more appropriate
    name.

--- navigator.js
+++ navigator.js
@@ -4,7 +4,6 @@
 
 const Extension = imports.misc.extensionUtils.extensions['paperwm@hedning:matrix.org'];
 const SwitcherPopup = imports.ui.switcherPopup;
-const WindowManager = imports.ui.windowManager;
 const Lang = imports.lang;
 const Meta = imports.gi.Meta;
 const Main = imports.ui.main;
@@ -250,7 +249,7 @@ var PreviewedWindowNavigator = new Lang.Class({
 
         let multimap = this.multimap;
         let last = multimap.minimaps[multimap.selectedIndex - 1];
-        Main.wm._previewWorkspace(
+        switchWorkspace(
             last && last.space.workspace,
             this.space.workspace,
             () => {
@@ -290,8 +289,8 @@ var PreviewedWindowNavigator = new Lang.Class({
             let multimap = this.multimap;
             let last = multimap.minimaps[multimap.selectedIndex - 1];
             if (focus.get_workspace() !== this.space.workspace) {
-                Main.wm._previewWorkspace(last && last.space.workspace,
-                                          focus.get_workspace());
+                switchWorkspace(last && last.space.workspace,
+                                focus.get_workspace());
             }
             Tiling.ensure_viewport(Tiling.spaces.spaceOfWindow(focus), focus);
         }
@@ -306,16 +305,12 @@ function preview_navigate(display, screen, meta_window, binding) {
     tabPopup.show(binding.is_reversed(), binding.get_name(), binding.get_mask())
 }
 
-
-WindowManager.WindowManager.prototype._previewWorkspace = function(from, to, callback) {
-
+function switchWorkspace(from, to, callback) {
     TopBar.updateWorkspaceIndicator(to.index());
 
     let xDest = 0, yDest = global.screen_height;
 
     let toSpace = Tiling.spaces.spaceOf(to);
-
-    this._toSpace = toSpace;
 
     toSpace.forEach(w => {
         w.get_compositor_private().hide();
@@ -336,7 +331,6 @@ WindowManager.WindowManager.prototype._previewWorkspace = function(from, to, cal
         return;
 
     let fromSpace = Tiling.spaces.spaceOf(from) || [];
-    this._fromSpace = fromSpace;
 
     let cloneParent = fromSpace.cloneContainer.get_parent();
     cloneParent.set_child_below_sibling(
