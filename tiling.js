47110d8f8268945520fd2b64ff8b1ea3505d9ace
Tor Hedin Brønner <torhedinbronner@gmail.com>
2018-05-01 16:07:03 +0200

    resize: Simplify by using move_to

--- tiling.js
+++ tiling.js
@@ -1106,26 +1106,8 @@ function sizeHandler(metaWindow) {
     if (space.selectedWindow !== metaWindow)
         return;
 
-    let index = space.indexOf(metaWindow);
     let frame = metaWindow.get_frame_rect();
-    let monitor = space.monitor;
-    frame.x -= monitor.x; frame.y -= monitor.y;
-
-    space.monitor.clickOverlay.reset();
-    propagateForward(space, index + 1, frame.x + frame.width + window_gap);
-    propagateBackward(space, index - 1, frame.x - window_gap);
-
-    // Sync clone position
-    let clone = metaWindow.clone;
-    if (clone.visible) {
-        // Note we can't rely on the actors position being set here.
-        let buffer = metaWindow.get_buffer_rect();
-        let x_offset = frame.x - buffer.x;
-        let y_offset = frame.y - buffer.y;
-        Tweener.removeTweens(clone);
-        clone.set_position(frame.x - monitor.x - x_offset,
-                           frame.y - monitor.y - y_offset);
-    }
+    move_to(space, metaWindow, {x: frame.x, y: frame.y});
 }
 
 // `MetaWindow::focus` handling
