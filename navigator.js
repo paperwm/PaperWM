paperwm/PaperWM

commit a8efb66aa52184af5b5272466ac484c208abc614

Force ensure on all workspace switches
Also don't emit 'move-done' if we've looked in the workspace stack.

navigator.js
@@ -362,7 +362,7 @@ var PreviewedWindowNavigator = new Lang.Class({
             this.space.setMonitor(this.monitor, true);
         }
 
-        if (this.space.delayed)
+        if (this.space.delayed && !force)
             this.space.emit('move-done');
 
         for (let monitor of Main.layoutManager.monitors) {
@@ -380,7 +380,6 @@ var PreviewedWindowNavigator = new Lang.Class({
         if (selected && !Scratch.isScratchActive()) {
             Main.activateWindow(selected);
             debug('#preview', 'Finish', selected.title, this._selectedIndex);
-            Tiling.ensureViewport(selected, this.space, force);
         } else {
             this.space.workspace.activate(global.get_current_time());
         }
@@ -413,6 +412,9 @@ function switchWorkspace(to, from, callback) {
     let xDest = 0, yDest = global.screen_height;
 
     let toSpace = Tiling.spaces.spaceOf(to);
+    let selected = toSpace.selectedWindow;
+    if (selected)
+        Tiling.ensureViewport(selected, this.space, true);
 
     if (from) {
         Tiling.spaces.spaceOf(from).forEach(w => {

commit 2aa1f84b330809fe26622ce91b4d45592f25fa58

Bugfix: Canceling navigator didn't return to focused window
Regression after a8efb66aa52184af5b5272466ac484c208abc614

navigator.js
@@ -367,13 +367,20 @@ var PreviewedWindowNavigator = new Lang.Class({
         }
 
         if (this.space === from && force) {
-            // We can't activate an already active workspace
+            // Animate the selected space into full view - normally this
+            // happens on workspace switch, but activating the same workspace
+            // again doesn't trigger a switch signal
             switchWorkspace(this.space.workspace);
         }
 
         let selected = this.space.selectedWindow;
         if (selected && !Scratch.isScratchActive()) {
-            Main.activateWindow(selected);
+            if (selected !== global.display.focus_window) {
+                Main.activateWindow(selected);
+            } else {
+                // Typically on cancel - just make sure the window is in view
+                Tiling.ensureViewport(selected, this.space); 
+            }
             debug('#preview', 'Finish', selected.title, this._selectedIndex);
         } else {
             this.space.workspace.activate(global.get_current_time());