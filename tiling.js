paperwm/PaperWM

commit 8d3ae51728efbcd48b4a49a7d7e05f0b34d61f4b

Only raise after focus

tiling.js
@@ -876,12 +876,12 @@ function ensureViewport(meta_window, space, force) {
     space = space || spaces.spaceOfWindow(meta_window);
     if (space.moving == meta_window && !force) {
         debug('already moving', meta_window.title);
-        return;
+        return undefined;
     }
 
     let index = space.indexOf(meta_window)
     if (index === -1)
-        return;
+        return undefined;
 
     debug('Moving', meta_window.title);
     meta_window._isStacked = false;
@@ -1027,7 +1027,6 @@ function move_to(space, meta_window, { x, y, delay, transition,
 
     propagateForward(space, index + 1, x + frame.width + window_gap);
     propagateBackward(space, index - 1, x - window_gap);
-    fixStack(space, index);
 }
 
 // Place window's left edge at x
@@ -1133,7 +1132,7 @@ function focus_handler(meta_window, user_data) {
     }
 
     let space = spaces.spaceOfWindow(meta_window);
-    ensureViewport(meta_window, space);
+    ensureViewport(meta_window, space) !== undefined && fixStack(space, space.indexOf(meta_window));
 }
 let focus_wrapper = utils.dynamic_function_ref('focus_handler', Me);
 

commit b703bbb0b06824aa5febfe13b0d4e04ff2d27716

Bugfix: fixStack not always called when window receives focus
The primary symptom is that the wrong window is focused next when the current
window is closed. Most easily triggered by quickly letting <super> go when
navigation to the window (from far away).

I'm not sure why the fixStack call was conditioned in the first place?

Probably regression after: 8d3ae51728efbcd48b4a49a7d7e05f0b34d61f4b

tiling.js
@@ -1189,7 +1189,8 @@ function focus_handler(meta_window, user_data) {
     }
 
     let space = spaces.spaceOfWindow(meta_window);
-    ensureViewport(meta_window, space) !== undefined && fixStack(space, space.indexOf(meta_window));
+    ensureViewport(meta_window, space);
+    fixStack(space, space.indexOf(meta_window));
 }
 let focus_wrapper = utils.dynamic_function_ref('focus_handler', Me);
 
@@ -1241,6 +1242,7 @@ let showWrapper = utils.dynamic_function_ref('showHandler', Me);
   stack, not the mru, when auto choosing focus after closing a window.
  */
 function fixStack(space, around) {
+    utils.debug("stack", "fix stack");
     let mru = global.display.get_tab_list(Meta.TabList.NORMAL,
                                           space.workspace);
 