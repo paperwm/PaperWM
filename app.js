33a365f326fb914a5e96b7c10ea9c18f51817e6b
Ole Jørgen Brønner <olejorgenb@yahoo.no>
2018-05-21 14:18:09 +0200

    Handle gnome-terminal specially in `newWindow`
    Invoking `app.open_new_window` on gnome-terminal does not inherit $PWD. We want
    that so take matter into our own hands.
    
    Note: vte.sh must be sourced in the terminal's shell.
    
    Other approaches:
    
    Write a custom .desktop file where "Exec" is a custom script (Note that it's
    probably not possible to call the dbus action using dbus-send or qdbus due to
    the argument structure)
    
    I tried to use the dbus interface directly from js too, but it seems basically
    impossible to call correctly..
    
      const Gio = imports.gi.Gio;
      const GLib = imports.gi.GLib;
      Gio.DBus.session.call(
          metaWindow.gtk_unique_bus_name,
          metaWindow.gtk_window_object_path,
          "org.gtk.Actions",
          "Activate",
          GLib.Variant.new_tuple([
              new GLib.Variant("s", "new-terminal"),
              GLib.Variant.new_array(null,
                 [new GLib.Variant('(ss)', ["window", "current"])]),
              GLib.Variant.new_array(new GLib.VariantType("{sv}"), [])
          ]),
          null, Gio.DBusCallFlags.NONE, -1, null,
          (conn, result) => {}
       );
    
    Complains about wrong type - wants the first array to be "av", not "a(ss)", but
    I don't know how to create a "opaque" variant..
    
    Keywords: current directory

--- app.js
+++ app.js
@@ -5,14 +5,40 @@
 var Shell = imports.gi.Shell;
 var Tracker = Shell.WindowTracker.get_default();
 
+function newGnomeTerminal(metaWindow, app) {
+    if (app.get_id() !== "org.gnome.Terminal.desktop") {
+        return false;
+    }
+
+    /* Note: this action activation is _not_ bound to the window - instead it
+       relies on the window being active when called. 
+
+       If the new window doesn't start in the same directory it's probably
+       because 'vte.sh' haven't been sourced by the shell in this terminal */
+    app.action_group.activate_action(
+        "win.new-terminal", new imports.gi.GLib.Variant("(ss)", ["window", "current"]));
+    return true;
+}
+
+function defaultHandler(metaWindow, app) {
+    if (!app.can_open_new_window()) {
+        return false;
+    }
+
+    let workspaceId = metaWindow.get_workspace().workspace_index;
+    app.open_new_window(workspaceId);
+    return true;
+}
+
+// Handlers are called in turn until one returns true
+var customHandlers = [newGnomeTerminal, defaultHandler];
+
 function newWindow(metaWindow) {
     metaWindow = metaWindow || global.display.focus_window;
     let app = Tracker.get_window_app(metaWindow);
-    if (app.can_open_new_window()) {
-        let workspaceId = metaWindow.get_workspace().workspace_index;
-        app.open_new_window(workspaceId);
+    for (let i = 0; i < customHandlers.length; i++) {
+        if (customHandlers[i](metaWindow, app)) {
+            break;
+        }
     }
-
 }
-
-
