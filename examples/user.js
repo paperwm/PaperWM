49f402c8d2d8561d1df1169be677326745447c63
Ole Jørgen Brønner <olejorgenb@yahoo.no>
2018-07-01 17:52:11 +0200

    Include the most common imports in user.js
    No need to split declaration and initialization any more either (?)

--- examples/user.js
+++ examples/user.js
@@ -1,16 +1,28 @@
 // -*- mode: gnome-shell -*-
 
-var Extension;
+var Meta = imports.gi.Meta;
+var Clutter = imports.gi.Clutter;
+var St = imports.gi.St;
+var Tweener = imports.ui.tweener;
+var Main = imports.ui.main;
+var Shell = imports.gi.Shell;
+
+// Extension local imports
+var Extension = imports.misc.extensionUtils.getCurrentExtension();
+var Me = Extension.imports.user;
+var Tiling = Extension.imports.tiling;
+var Utils = Extension.imports.utils;
+var App = Extension.imports.app;
+
 function init() {
-  // Runs _only_ once on startup
-  Extension = imports.misc.extensionUtils.getCurrentExtension();
+    // Runs _only_ once on startup
 }
 
 function enable() {
-  // Runs on extension reloads, eg. when unlocking the session
+    // Runs on extension reloads, eg. when unlocking the session
 }
 
 function disable() {
-  // Runs on extension reloads eg. when locking the session (`<super>L).
+    // Runs on extension reloads eg. when locking the session (`<super>L).
 }
 
