18b116c2c12db7924ff1b66ed88d5d6d52eb5aa8
Tor Hedin Brønner <torhedinbronner@gmail.com>
2018-07-03 18:02:16 +0200

    Don't let the selection highlight protrude into the top bar

--- settings.js
+++ settings.js
@@ -10,6 +10,12 @@ var prefs = {
     workspace_colors: settings.get_strv('workspace-colors')
 };
 
+function setVerticalMargin() {
+    let vMargin = settings.get_int('vertical-margin');
+    let gap = settings.get_int('window-gap');
+    prefs.vertical_margin = Math.max(Math.round(gap/2), vMargin);
+}
+
 function setState(_, key) {
     let value = settings.get_value(key);
     let name = key.replace(/-/g, '_');
@@ -24,5 +30,9 @@ function setState(_, key) {
 }
 
 function init() {
-    settings.connect('changed', setState);
+    settings.connect('changed::window-gap', setState);
+    settings.connect('changed::horizontal-margin', setState);
+    settings.connect('changed::vertical-margin', setVerticalMargin);
+    setVerticalMargin();
+    settings.connect('changed::workspace-colors', setState);
 }
