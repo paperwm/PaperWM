paperwm/PaperWM

commit 5d0f037d3ee4f6634b70b7bd74c2be77b895044f

Track the background actor during animation (X11)
This ensures all mouse input is redirected to clutter.

As #80 shows, weird things can happen when mouse clicks occurs in the region of
a "visible" X11 window with a hidden actor.

Fixes #80

tiling.js
@@ -611,10 +611,25 @@ class Space extends Array {
 
         this.fixOverlays();
 
+        if (!Meta.is_wayland_compositor()) {
+            // See startAnimate
+            Main.layoutManager.untrackChrome(this.background);
+        }
+
+        this._isAnimating = false;
+
         this.emit('move-done');
     }
 
     startAnimate(grabWindow) {
+
+        if (!this._isAnimating && !Meta.is_wayland_compositor()) {
+            // Tracking the background fixes issue #80
+            // It also let us activate window clones clicked during animation
+            // Untracked in moveDone
+            Main.layoutManager.trackChrome(this.background);
+        }
+
         this.visible.forEach(w => {
             let actor = w.get_compositor_private();
             if (!actor)
@@ -628,6 +643,8 @@ class Space extends Array {
             actor.hide();
             w.clone.show();
         });
+
+        this._isAnimating = true;
     }
 
     fixOverlays(metaWindow) {

commit 2137dddfa03e5b9f8347940bc5afb7f74d0b9c9f

Bugfix: SSD frame buttons (minimize, ...) stopped working after 5d0f037d3
button-press on these buttons trigger a grab (GrabOp.FRAME_BUTTON).

Our grab handler run startAnimate which tracks the background -> the
button-press is stolen

Only happened for server-side-decorated windows (make sense that the grab is not
triggered for CSD windows)

Regression after: 5d0f037d3

tiling.js
@@ -1941,8 +1941,8 @@ function move_to(space, metaWindow, { x, y, transition, force }) {
 
 var noAnimate = false;
 function grabBegin(metaWindow, type) {
-    // Don't handle pushModal grabs
-    if (type === Meta.GrabOp.COMPOSITOR)
+    // Don't handle pushModal grabs and SCD button (close/minimize/etc.) grabs
+    if (type === Meta.GrabOp.COMPOSITOR || type === Meta.GrabOp.FRAME_BUTTON)
         return;
     let space = spaces.spaceOfWindow(metaWindow);
     if (space.indexOf(metaWindow) === -1)

utils.js
@@ -40,7 +40,7 @@ function print_stacktrace(error) {
         return frame !== "wrapper@resource:///org/gnome/gjs/modules/lang.js:178"   
     });
     let args = [...arguments];
-    args.splice(0, 1, "#stacktrace:"+(args[0] ? args[0] : ""));
+    args.splice(0, 1, "#stacktrace", ":"+(args[0] ? args[0] : ""));
     // Use non-breaking space to encode new lines (otherwise every frame is
     // prefixed by timestamp)
     let nl = " ";
@@ -50,6 +50,18 @@ function print_stacktrace(error) {
 
 function framestr(rect) {
     return "[ x:"+rect.x + ", y:" + rect.y + " w:" + rect.width + " h:"+rect.height + " ]";
+}
+
+/**
+ * Returns a human-readable enum value representation
+ */
+function ppEnumValue(value, genum) {
+    let entry = Object.entries(genum).find(([k, v]) => v === value);
+    if (entry) {
+        return `${entry[0]} (${entry[1]})`
+    } else {
+        return `<not-found> (${value})`
+    }
 }
 
 /**