7ade1ef1de68fda218d0b0392118b79598ff90e1
Ole Jørgen Brønner <olejorgenb@yahoo.no>
2018-12-19 16:37:00 +0100

    Revert "Mark windows as unmapped while changing between monitors"
    This reverts commit 48ac0268f66072f1a813028bb24febf94cd3488f.
    
    Did not seem to resolve the issue and it sometimes causes windows to be stuck in
    a "unmapped" state. Eg. when suspending, resuming and unlocking on ubuntu 18.04
    gnome 3.28 windowEnteredMonitor is called on the visible windows - marking them
    as unmapped.

--- tiling.js
+++ tiling.js
@@ -1486,9 +1486,6 @@ class Spaces extends Map {
             || metaWindow.is_on_all_workspaces())
             return;
 
-        // Prevent the window from flip flopping between monitors
-        metaWindow.unmapped = true;
-
         let monitor = Main.layoutManager.monitors[index];
         let space = this.monitors.get(monitor);
         if (space.monitor !== monitor)
@@ -1680,7 +1677,6 @@ function add_handler(ws, metaWindow) {
         // Set position and hookup signals, with `existing` set to true
         insertWindow(metaWindow, {existing: true && !metaWindow.redirected});
         delete metaWindow.redirected;
-        delete metaWindow.unmapped;
     }
     // Otherwise we're dealing with a new window, so we let `window-created`
     // handle initial positioning.
