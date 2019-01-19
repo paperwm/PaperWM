f5f5d435fa7d6d968e857658e1bfd3f4b104de49
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-01-19 20:29:10 +0100

    install.sh: overwrite symlinks (but only symlinks)

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-ln -s "$(realpath .)" ~/'.local/share/gnome-shell/extensions/paperwm@hedning:matrix.org'
+DEST=${XDG_DATA_HOME:-$HOME/.local/share}/'gnome-shell/extensions/paperwm@hedning:matrix.org'
+
+if [[ -L "$DEST" || ! -e "$DEST" ]]; then
+    ln -fTs "$(realpath ./src)" "$DEST"
+else
+    echo Install destination already exists: "$DEST"
+    echo "  re run install to fix it."
+fi
