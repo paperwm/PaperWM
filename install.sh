dc970771d702b9f5d402d35e106b12575495a8ae
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-02-06 20:41:29 +0100

    installer: make sure the gnome shell extensions dir exists

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-ln -s "$(realpath .)" ~/'.local/share/gnome-shell/extensions/paperwm@hedning:matrix.org'
+EXT_DIR=${XDG_DATA_HOME:-$HOME/.local/share}/gnome-shell/extensions
+mkdir -p "$EXT_DIR"
+ln -s "$(realpath .)" "$EXT_DIR"/'paperwm@hedning:matrix.org'
