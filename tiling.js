f83eb0b839ed2e4483696722eaa36b7385e2655e
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-06 23:59:04 +0100

    fix crash: Only destroy mapped clones
    This caused crashed on 3.32 when closing scratch windows. Ie. windows where the
    clone isn't in the scene graph.

--- tiling.js
+++ tiling.js
@@ -1828,7 +1828,7 @@ function remove_handler(workspace, meta_window) {
     let actor = meta_window.get_compositor_private();
     if (!actor) {
         signals.disconnect(meta_window);
-        if (meta_window.clone)
+        if (meta_window.clone && meta_window.clone.mapped)
             meta_window.clone.destroy();
     }
 }
