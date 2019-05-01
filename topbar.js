paperwm/PaperWM

commit 20809ad6b0e7f764fcfbde2f13ec274f457e22d5

Disable 'affectsStruts' for the topbar
Caused problems when switching to workspaces without topbar - probably because
the topbar is briefly shown during navigation - restricting the workarea -
resizing the windows in the workspace without topbar - causing the dreaded #73.

This conjecture is not verified, but disabling the affectsStruts fixes the
symptoms :)

Fixes #108

topbar.js
@@ -421,6 +421,13 @@ var panelBoxShowId, panelBoxHideId;
 function enable () {
     Main.panel.statusArea.activities.actor.hide();
 
+    // See #108
+    Main.layoutManager.removeChrome(Main.layoutManager.panelBox);
+    Main.layoutManager.addChrome(Main.layoutManager.panelBox, {
+        affectsStruts: false,
+        trackFullscreen: true
+    });
+
     menu = new WorkspaceMenu();
     signals.connect(menu._label, 'notify::allocation', (label) => {
         let point = new Clutter.Vertex({x: 0, y: 0});
@@ -480,6 +487,12 @@ function disable() {
     screenSignals = [];
 
     panelBox.scale_y = 1;
+
+    Main.layoutManager.removeChrome(Main.layoutManager.panelBox);
+    Main.layoutManager.addChrome(Main.layoutManager.panelBox, {
+        affectsStruts: true,
+        trackFullscreen: true
+    });
 }
 
 function show() {

commit 86283c5deaed3c926395314987aa6e094b450d58

Regression: notifications and topbar overlaps
Monkeypatch notification system to position notifications relative to topbar
instead of workarea.

See https://gitlab.gnome.org/GNOME/gnome-shell/blob/8572bb97c70821bd82dbe5cca78071b474eb33c4/js/ui/messageTray.js#L830

Regression after 20809ad6b0e7f764fcfbde2f13ec274f457e22d5
Fixes #109

topbar.js
@@ -418,6 +418,7 @@ function init () {
 }
 
 var panelBoxShowId, panelBoxHideId;
+var originalMessageTrayTween;
 function enable () {
     Main.panel.statusArea.activities.actor.hide();
 
@@ -427,6 +428,14 @@ function enable () {
         affectsStruts: false,
         trackFullscreen: true
     });
+
+    // See #109
+    originalMessageTrayTween = Main.messageTray._tween;
+    Main.messageTray._tween = function(actor, statevar, value, params) {
+        if (panelBox.visible)
+            params.y += panelBox.height;
+        originalMessageTrayTween.apply(Main.messageTray, arguments);
+    }
 
     menu = new WorkspaceMenu();
     signals.connect(menu._label, 'notify::allocation', (label) => {
@@ -493,6 +502,8 @@ function disable() {
         affectsStruts: true,
         trackFullscreen: true
     });
+
+    Main.messageTray._tween = originalMessageTrayTween;
 }
 
 function show() {