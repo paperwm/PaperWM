528154db4e96ad0e2363bf215411936ed75960d7
Ole Jørgen Brønner <olejorgenb@yahoo.no>
2019-10-12 16:47:00 +0200

    app: launchFromWorkspaceDir util

--- app.js
+++ app.js
@@ -2,12 +2,47 @@
   Application functionality, like global new window actions etc.
  */
 
+var Extension;
+if (imports.misc.extensionUtils.extensions) {
+    Extension = imports.misc.extensionUtils.extensions["paperwm@hedning:matrix.org"];
+} else {
+    Extension = imports.ui.main.extensionManager.lookup("paperwm@hedning:matrix.org");
+}
+
+var GLib = imports.gi.GLib
+var Gio = imports.gi.Gio;
+var Tiling = Extension.imports.tiling
+
 var Shell = imports.gi.Shell;
 var Tracker = Shell.WindowTracker.get_default();
 
+var CouldNotLaunch = Symbol();
+
+function launchFromWorkspaceDir(app, workspace) {
+    let space = Tiling.spaces.get(workspace);
+    let dir = space.settings.get_string("directory");
+    let cmd = app.app_info.get_commandline();
+    if (!cmd || dir == '') {
+        throw CouldNotLaunch;
+    }
+
+    if (dir[0] == "~") {
+        dir = GLib.getenv("HOME") + dir.slice(1);
+    }
+
+    // TODO: substitute correct values according to https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html#exec-variables
+    cmd = cmd.replace(/%./g, "");
+    let [success, cmdArgs] = GLib.shell_parse_argv(cmd);
+    if (!success) {
+        print("launchFromWorkspaceDir:", "Could not parse command line", cmd);
+        throw CouldNotLaunch;
+    }
+    GLib.spawn_async(dir, cmdArgs, GLib.get_environ(), GLib.SpawnFlags.SEARCH_PATH, null);
+}
+
 function newGnomeTerminal(metaWindow, app) {
     /* Note: this action activation is _not_ bound to the window - instead it
-       relies on the window being active when called. 
+       relies on the window being active when called.
 
        If the new window doesn't start in the same directory it's probably
        because 'vte.sh' haven't been sourced by the shell in this terminal */
@@ -23,6 +58,10 @@ function defaultHandler(metaWindow, app) {
     let workspaceId = metaWindow.get_workspace().workspace_index;
     app.open_new_window(workspaceId);
     return true;
+}
+
+function startFromWorkspaceDirHandler(metaWindow, app) {
+    return launchFromWorkspaceDir(app, metaWindow.get_workspace());
 }
 
 // Lookup table for custom handlers, keys being the app id
