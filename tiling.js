7841e7368b40047d310adce1094a9c66bc91dc79
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-12-10 06:12:59 +0100

    Highlight: Do not protrude when maximized horizontally

--- tiling.js
+++ tiling.js
@@ -2098,11 +2098,13 @@ function allocateClone(metaWindow) {
 
     if (metaWindow.clone.first_child.name === 'selection') {
         let selection = metaWindow.clone.first_child;
-        let maximized = metaWindow.maximized_vertically;
+        let vMax = metaWindow.maximized_vertically;
+        let hMax = metaWindow.maximized_horizontally;
         let protrusion = Math.round(prefs.window_gap/2);
-        selection.x = - protrusion;
-        selection.y = maximized ? 0 : - protrusion;
-        selection.set_size(frame.width + prefs.window_gap, frame.height + (maximized ? 0 : prefs.window_gap));
+        selection.x = hMax ? 0 : - protrusion;
+        selection.y = vMax ? 0 : - protrusion;
+        selection.set_size(frame.width + (hMax ? 0 : prefs.window_gap),
+                           frame.height + (vMax ? 0 : prefs.window_gap));
     }
 }
 
