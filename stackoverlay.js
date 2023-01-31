ed4d9dbf21f060d9d1f643b889c066ca431662fa
Jay Ta'ala <jay@jaytaala.com>
2023-01-31 21:07:02 +1100

    Opens next window preview (if exists) when clicking with current preview. Note, removes timeout limit on preview show as long as mouse in against window edge.

--- stackoverlay.js
+++ stackoverlay.js
@@ -228,13 +228,17 @@ var StackOverlay = class StackOverlay {
                 this.clone.destroy();
                 this.clone = null;
             }
+
+            // remove/cleanup the previous preview
+            this.removePreview();
+            Mainloop.timeout_add(200, this.triggerPreview.bind(this));
             return true;
         });
 
         this.signals.connect(overlay, 'enter-event', this.triggerPreview.bind(this));
         this.signals.connect(overlay,'leave-event', this.removePreview.bind(this));
         this.signals.connect(Settings.settings, 'changed::pressure-barrier',
-                             this.updateBarrier.bind(this, true));
+            this.updateBarrier.bind(this, true));
 
         this.updateBarrier();
 
@@ -275,7 +279,8 @@ var StackOverlay = class StackOverlay {
             clone.set_position(x, y);
         });
 
-        this._removeId = Mainloop.timeout_add_seconds(2, this.removePreview.bind(this));
+        // uncomment to remove the preview after a timeout
+        //this._removeId = Mainloop.timeout_add_seconds(2, this.removePreview.bind(this));
     }
 
     removePreview() {
