8d546550b043ee8227c84112e19f9e8e37e8b261
Matthias Seiffert <matthias-seiffert@hotmail.de>
2023-04-16 19:54:35 +0200

    Add fallback for gnome-extensions command

--- gather-system-info.sh
+++ gather-system-info.sh
@@ -9,36 +9,99 @@
 # - awk
 
 REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "${REPO}"
 
-echo "Please include this information in your bug report on GitHub!"
+main() {
+    cd "${REPO}"
 
-echo -n "Distribution: "
-if [ -f /etc/os-release ]; then
-    source /etc/os-release && echo "${NAME}"
-fi
+    echo "Please include this information in your bug report on GitHub!"
 
-gnome-shell --version
+    show_distribution
+    show_gnome_version
+    show_display_server
+    show_paperwm_version
+    show_gnome_extensions
+}
 
-echo -n "Display server: "
-if [ "${XDG_SESSION_TYPE}" = "wayland" ]; then
-    echo "Wayland"
-else
-    echo "Xorg"
-fi
-
-echo -n "PaperWM branch/tag: "
-git symbolic-ref --short -q HEAD || git name-rev --tags --name-only --no-undefined "$(git rev-parse HEAD)"
-echo -n "PaperWM commit: "
-git rev-parse HEAD
-
-echo "Enabled extensions:"
-# make a markdown list out of gnome-extensions list
-gnome-extensions list --enabled | {
-    if command -v awk >/dev/null; then
-        awk '{print "- " $0}'
-    else
-        cat
+show_distribution() {
+    echo -n "Distribution: "
+    if [ -f /etc/os-release ]; then
+        source /etc/os-release && echo "${NAME}"
     fi
 }
 
+show_gnome_version() {
+    gnome-shell --version
+}
+
+show_display_server() {
+    echo -n "Display server: "
+    if [ "${XDG_SESSION_TYPE}" = "wayland" ]; then
+        echo "Wayland"
+    else
+        echo "Xorg"
+    fi
+}
+
+show_paperwm_version() {
+    echo -n "PaperWM branch/tag: "
+    git symbolic-ref --short -q HEAD || git name-rev --tags --name-only --no-undefined "$(git rev-parse HEAD)"
+    echo -n "PaperWM commit: "
+    git rev-parse HEAD
+
+}
+
+show_gnome_extensions() {
+    echo "Enabled extensions:"
+    # make a markdown list out of gnome-extensions list
+    # use gnome-extensions if it exists and falls back to bash on older gnome
+    # versions
+    if command -v gnome-extensions >/dev/null; then
+        gnome-extensions list --enabled | {
+            if command -v awk >/dev/null; then
+                awk '{print "- " $0}'
+            else
+                cat
+            fi
+        }
+    else
+        # compare enabled extensions to installed extensions
+        # because some uninstalled extensions could still be enabled because of
+        # stale gsettings
+        local enabled=$(_enabled_extensions)
+        local installed=$(_installed_extensions)
+
+        for ext in $enabled; do
+            if [[ "${installed}" =~ "${ext}" ]]; then
+                echo "- $ext"
+            fi
+        done
+    fi
+}
+
+_installed_extensions() {
+    ls ~/.local/share/gnome-shell/extensions/
+    ls /usr/share/gnome-shell/extensions/
+}
+
+_enabled_extensions() {
+    local s matches
+    s=$(gsettings get org.gnome.shell enabled-extensions)
+    # $matches contains lines with the extension uuid and lines with ", "
+    mapfile -t matches < <(_global_rematch "$s" "'([^']*)'")
+    for match in "${matches[@]}"; do
+        if [[ "$match" =~ "^," ]]; then
+            continue
+        fi
+        echo "$match"
+    done
+}
+
+_global_rematch() {
+    local s=$1 regex=$2
+    while [[ $s =~ $regex ]]; do
+        echo "${BASH_REMATCH[1]}"
+        s=${s#*"${BASH_REMATCH[1]}"}
+    done
+}
+
+main "$@"
