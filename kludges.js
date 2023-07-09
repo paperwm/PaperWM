b058d2265ce859bef0256f909314d766ad94fbef
Jay Ta'ala <jay@jaytaala.com>
2023-07-09 17:39:36 +1000

    Moved prototype overriding functions to register (where they should be). Clean-up MessageTray (not need anymore).

--- kludges.js
+++ kludges.js
@@ -17,7 +17,6 @@ var WorkspaceAnimation = imports.ui.workspaceAnimation;
 var Shell = imports.gi.Shell;
 var utils = Extension.imports.utils;
 var Params = imports.misc.params;
-var MessageTray = imports.ui.messageTray;
 
 var Scratch = Extension.imports.scratch;
 var Tiling = Extension.imports.tiling;
@@ -32,50 +31,6 @@ function overrideHotCorners() {
 
         corner._pressureBarrier._trigger = function() {};
     }
-}
-
-// Workspace.Workspace._realRecalculateWindowPositions
-// Sort tiled windows in the correct order
-function _realRecalculateWindowPositions(flags) {
-    if (this._repositionWindowsId > 0) {
-        Mainloop.source_remove(this._repositionWindowsId);
-        this._repositionWindowsId = 0;
-    }
-
-    let clones = this._windows.slice();
-    if (clones.length === 0) {
-        return;
-    }
-
-    let space = Tiling.spaces.spaceOf(this.metaWorkspace);
-    if (space) {
-        clones.sort((a, b) => {
-            let aw = a.metaWindow;
-            let bw = b.metaWindow;
-            let ia = space.indexOf(aw);
-            let ib = space.indexOf(bw);
-            if (ia === -1 && ib === -1) {
-                return a.metaWindow.get_stable_sequence() - b.metaWindow.get_stable_sequence();
-            }
-            if (ia === -1) {
-                return -1;
-            }
-            if (ib === -1) {
-                return 1;
-            }
-            return ia - ib;
-        });
-    } else {
-        clones.sort((a, b) => {
-            return a.metaWindow.get_stable_sequence() - b.metaWindow.get_stable_sequence();
-        });
-    }
-
-    if (this._reservedSlot)
-        clones.push(this._reservedSlot);
-
-    this._currentLayout = this._computeLayout(clones);
-    this._updateWindowPositions(flags);
 }
 
 // Workspace.WindowClone.getOriginalPosition
@@ -214,32 +169,91 @@ function startup() {
         Main?.overview?._swipeTracker, // gnome 40+
         Main?.overview?._overview?._controls?._workspacesDisplay?._swipeTracker, // gnome 40+
         Main?.wm?._workspaceAnimation?._swipeTracker, // gnome 40+
-        Main?.wm?._swipeTracker // gnome 38 (and below)
+        Main?.wm?._swipeTracker, // gnome 38 (and below)
     ].filter(t => typeof t !== 'undefined');
 
-    registerOverridePrototype(MessageTray.MessageTray, '_updateState');
-    registerOverridePrototype(WindowManager.WindowManager, '_prepareWorkspaceSwitch');
+    // prepare for PaperWM workspace switching
+    registerOverridePrototype(WindowManager.WindowManager, '_prepareWorkspaceSwitch', function (from, to, direction) {
+        if (this._switchData)
+            return;
+
+        let switchData = {};
+        this._switchData = switchData;
+        switchData.movingWindowBin = new Clutter.Actor();
+        switchData.windows = [];
+        switchData.surroundings = {};
+        switchData.gestureActivated = false;
+        switchData.inProgress = false;
+
+        switchData.container = new Clutter.Actor();
+    });
+
     registerOverridePrototype(WorkspaceAnimation.WorkspaceAnimationController, 'animateSwitch', animateSwitch);
     registerOverridePrototype(Workspace.Workspace, '_isOverviewWindow');
     if (Workspace.WindowClone)
         registerOverridePrototype(Workspace.WindowClone, 'getOriginalPosition', getOriginalPosition);
 
-    registerOverridePrototype(Workspace.Workspace, '_realRecalculateWindowPositions', _realRecalculateWindowPositions);
+    // Workspace.Workspace._realRecalculateWindowPositions
+    // Sort tiled windows in the correct order
+    registerOverridePrototype(Workspace.Workspace, '_realRecalculateWindowPositions',
+
+        function (flags) {
+            if (this._repositionWindowsId > 0) {
+                Mainloop.source_remove(this._repositionWindowsId);
+                this._repositionWindowsId = 0;
+            }
+
+            let clones = this._windows.slice();
+            if (clones.length === 0) {
+                return;
+            }
+
+            let space = Tiling.spaces.spaceOf(this.metaWorkspace);
+            if (space) {
+                clones.sort((a, b) => {
+                    let aw = a.metaWindow;
+                    let bw = b.metaWindow;
+                    let ia = space.indexOf(aw);
+                    let ib = space.indexOf(bw);
+                    if (ia === -1 && ib === -1) {
+                        return a.metaWindow.get_stable_sequence() - b.metaWindow.get_stable_sequence();
+                    }
+                    if (ia === -1) {
+                        return -1;
+                    }
+                    if (ib === -1) {
+                        return 1;
+                    }
+                    return ia - ib;
+                });
+            } else {
+                clones.sort((a, b) => {
+                    return a.metaWindow.get_stable_sequence() - b.metaWindow.get_stable_sequence();
+                });
+            }
+
+            if (this._reservedSlot)
+                clones.push(this._reservedSlot);
+
+            this._currentLayout = this._computeLayout(clones);
+            this._updateWindowPositions(flags);
+        });
 
     registerOverridePrototype(Workspace.UnalignedLayoutStrategy, '_sortRow', row => row);
 
     registerOverridePrototype(WindowManager.WorkspaceTracker, '_checkWorkspaces', _checkWorkspaces);
+
     if (WindowManager.TouchpadWorkspaceSwitchAction) // disable 4-finger swipe
         registerOverridePrototype(WindowManager.TouchpadWorkspaceSwitchAction, '_checkActivated', () => false);
 
     // Work around https://gitlab.gnome.org/GNOME/gnome-shell/issues/1884
     if (!WindowManager.WindowManager.prototype._removeEffect) {
         registerOverridePrototype(WindowManager.WindowManager, '_mapWindowOverwrite',
-                                  function (shellwm, actor) {
-                                      if (this._mapping.delete(actor)) {
-                                          shellwm.completed_map(actor);
-                                      }
-                                  });
+            function (shellwm, actor) {
+                if (this._mapping.delete(actor)) {
+                    shellwm.completed_map(actor);
+                }
+            });
     }
 
     let layout = computeLayout40;
@@ -250,7 +264,7 @@ function startup() {
         registerOverrideProp(t, "enabled", false);
     });
 
-    registerOverridePrototype(Workspace.Workspace, '_isOverviewWindow', (win) => {
+    registerOverridePrototype(Workspace.Workspace, '_isOverviewWindow', win => {
         let metaWindow = win.meta_window || win;
         if (settings.get_boolean('only-scratch-in-overview'))
             return Scratch.isScratchWindow(metaWindow) && !metaWindow.skip_taskbar;
@@ -277,7 +291,7 @@ function enable() {
             return true;
         }
     });
-    actions.forEach(a => global.stage.remove_action(a))
+    actions.forEach(a => global.stage.remove_action(a));
 
     signals.connect(settings, 'changed::override-hot-corner', disableHotcorners);
     disableHotcorners();
@@ -287,7 +301,9 @@ function enable() {
      * ensure swipe trackers are reset.
      */
     signals.connect(Main.overview, 'hidden', () => {
-        swipeTrackers.forEach(t => t.enabled = false);
+        swipeTrackers.forEach(t => {
+            t.enabled = false;
+        });
     });
 
     function scratchInOverview() {
@@ -302,36 +318,11 @@ function enable() {
     signals.connect(settings, 'changed::only-scratch-in-overview', scratchInOverview);
     signals.connect(settings, 'changed::disable-scratch-in-overview', scratchInOverview);
     scratchInOverview();
-
-
-    /* The «native» workspace animation can be now (3.30) be disabled as it
-       calls out of the function bound to the `switch-workspace` signal.
-     */
-    WindowManager.WindowManager.prototype._prepareWorkspaceSwitch =
-        function (from, to, direction) {
-            if (this._switchData)
-                return;
-
-            let wgroup = global.window_group;
-            let windows = global.get_window_actors();
-            let switchData = {};
-
-            this._switchData = switchData;
-            switchData.movingWindowBin = new Clutter.Actor();
-            switchData.windows = [];
-            switchData.surroundings = {};
-            switchData.gestureActivated = false;
-            switchData.inProgress = false;
-
-            switchData.container = new Clutter.Actor();
-        };
-
-    Workspace.Workspace.prototype._realRecalculateWindowPositions = _realRecalculateWindowPositions;
 }
 
 function disable() {
     disableOverrides();
-    actions.forEach(a => global.stage.add_action(a))
+    actions.forEach(a => global.stage.add_action(a));
 
     signals.destroy();
     signals = null;
@@ -345,11 +336,11 @@ function disable() {
 function sortWindows(a, b) {
     let aw = a.metaWindow;
     let bw = b.metaWindow;
-    let spaceA = Tiling.spaces.spaceOfWindow(aw)
-    let spaceB = Tiling.spaces.spaceOfWindow(bw)
+    let spaceA = Tiling.spaces.spaceOfWindow(aw);
+    let spaceB = Tiling.spaces.spaceOfWindow(bw);
     let ia = spaceA.indexOf(aw);
     let ib = spaceB.indexOf(bw);
-    if ((ia === -1 && ib === -1)) {
+    if (ia === -1 && ib === -1) {
         return a.metaWindow.get_stable_sequence() - b.metaWindow.get_stable_sequence();
     }
     if (ia === -1) {
@@ -423,7 +414,7 @@ function computeLayout40(windows, layoutParams) {
         rows,
         maxColumns: maxRow.windows.length,
         gridWidth: maxRow.fullWidth,
-        gridHeight
+        gridHeight,
     };
 }
 
