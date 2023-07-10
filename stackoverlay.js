737a707f05cd1d5f35555f296cfe614fcd22ec67
Jay Ta'ala <jay@jaytaala.com>
2023-07-10 21:04:10 +1000

    [21] WIP.

--- stackoverlay.js
+++ stackoverlay.js
@@ -1,13 +1,10 @@
 var Extension = imports.misc.extensionUtils.getCurrentExtension();
 var Tiling = Extension.imports.tiling;
-var Clutter = imports.gi.Clutter;
-var Main = imports.ui.main;
 var Mainloop = imports.mainloop;
-var Shell = imports.gi.Shell;
-var Meta = imports.gi.Meta;
+var {Clutter, Shell, Meta, St} = imports.gi;
+var Main = imports.ui.main;
 var utils = Extension.imports.utils;
 var Layout = imports.ui.layout;
-
 
 var Settings = Extension.imports.settings;
 var prefs = Settings.prefs;
@@ -50,8 +47,10 @@ function createAppIcon(metaWindow, size) {
     let tracker = Shell.WindowTracker.get_default();
     let app = tracker.get_window_app(metaWindow);
     let appIcon = app ? app.create_icon_texture(size)
-        : new St.Icon({ icon_name: 'icon-missing',
-                        icon_size: size });
+        : new St.Icon({
+            icon_name: 'icon-missing',
+            icon_size: size,
+        });
     appIcon.x_expand = appIcon.y_expand = true;
     appIcon.x_align = appIcon.y_align = Clutter.ActorAlign.END;
 
@@ -191,17 +190,19 @@ var ClickOverlay = class ClickOverlay {
             actor.destroy();
             overlay.removeBarrier();
         }
+        Main.layoutManager.untrackChrome(this.enterMonitor);
         this.enterMonitor.destroy();
     }
-}
+};
 
 var StackOverlay = class StackOverlay {
     constructor(direction, monitor) {
-
         this._direction = direction;
 
-        let overlay = new Clutter.Actor({ reactive: true
-                                          , name: "stack-overlay" });
+        let overlay = new Clutter.Actor({
+            reactive: true,
+            name: "stack-overlay",
+        });
 
         // Uncomment to debug the overlays
         // overlay.background_color = Clutter.color_from_string('green')[1];
@@ -235,7 +236,7 @@ var StackOverlay = class StackOverlay {
         });
 
         this.signals.connect(overlay, 'enter-event', this.triggerPreview.bind(this));
-        this.signals.connect(overlay,'leave-event', this.removePreview.bind(this));
+        this.signals.connect(overlay, 'leave-event', this.removePreview.bind(this));
         this.signals.connect(Settings.settings, 'changed::pressure-barrier',
             this.updateBarrier.bind(this, true));
 
@@ -305,11 +306,11 @@ var StackOverlay = class StackOverlay {
 
         // set clone parameters
         let scale = prefs.minimap_scale;
-        clone.opacity = 255*0.95;
-        
+        clone.opacity = 255 * 0.95;
+
         clone.set_scale(scale, scale);
         Main.uiGroup.add_actor(clone);
-        
+
         let monitor = this.monitor;
         let scaleWidth = scale * clone.width;
         let scaleHeight = scale * clone.height;
@@ -366,7 +367,7 @@ var StackOverlay = class StackOverlay {
         if (this.barrier || !prefs.pressure_barrier)
             return;
 
-        this.pressureBarrier = new Layout.PressureBarrier(100, 0.25*1000, Shell.ActionMode.NORMAL);
+        this.pressureBarrier = new Layout.PressureBarrier(100, 0.25 * 1000, Shell.ActionMode.NORMAL);
         // Show the overlay on fullscreen windows when applying pressure to the edge
         // The above leave-event handler will take care of hiding the overlay
         this.pressureBarrier.connect('trigger', () => {
@@ -384,17 +385,17 @@ var StackOverlay = class StackOverlay {
         let x1, directions;
         if (this._direction === Meta.MotionDirection.LEFT) {
             x1 = monitor.x,
-            directions = Meta.BarrierDirection.POSITIVE_X;
+                directions = Meta.BarrierDirection.POSITIVE_X;
         } else {
             x1 = monitor.x + monitor.width - 1,
-            directions = Meta.BarrierDirection.NEGATIVE_X;
+                directions = Meta.BarrierDirection.NEGATIVE_X;
         }
         this.barrier = new Meta.Barrier({
             display: global.display,
             x1, x2: x1,
             y1: workArea.y + 1,
             y2: workArea.y + workArea.height - 1,
-            directions
+            directions,
         });
         this.pressureBarrier.addBarrier(this.barrier);
     }
@@ -423,8 +424,6 @@ var StackOverlay = class StackOverlay {
             return;
 
         let overlay = this.overlay;
-        let actor = metaWindow.get_compositor_private();
-
         overlay.y = this.monitor.y + Main.layoutManager.panelBox.height + prefs.vertical_margin;
 
         // Assume the resize edge is at least this big (empirically found..)
