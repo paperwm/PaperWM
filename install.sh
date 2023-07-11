ac10f71f866a25b5c3aaa2d02ae5054303a81f24
Jay Ta'ala <jay@jaytaala.com>
2023-07-12 08:51:28 +1000

    Large simplification of the `install.sh` script.  Auto enabling hasn't worked for a long time and just confuses users.  The script now check if a non-symlinked version exists (e.g. one from extensions.gnome.org) and if not, apply the symlink with directions to the user on how to enable it.

--- install.sh
+++ install.sh
@@ -3,68 +3,34 @@
 REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 UUID=paperwm@paperwm.github.com
 EXT_DIR=${XDG_DATA_HOME:-$HOME/.local/share}/gnome-shell/extensions
+EXT="$EXT_DIR"/"$UUID"
 mkdir -p "$EXT_DIR"
-ln -sn "$REPO" "$EXT_DIR"/"$UUID"
 
-echo
-read -p "Enable the extension [Y/n]? " consent
-case "$consent" in
-    (Y|y|"")
-    ;;
-    *)
-        exit
-    ;;
-esac
+# Check if ext path already exists and if is a folder
+if [[ ! -L "$EXT" && -d "$EXT" ]]; then
+cat << EOF
 
-# Coax gnome-shell to enable the extension, since gnome-extensions enable does't
-# work without a restart
-ENABLE=`cat <<EOF
-try {
-    let path = "$EXT_DIR";
-    let uuid = "$UUID";
-    let Gio = imports.gi.Gio;
-    let extensionUtils, extensionSystem, paperwm;
-    // Work around differences between 3.32 and 3.34
-    if (imports.misc.extensionUtils.createExtensionObject) {
-        extensionSystem = imports.ui.extensionSystem;
-        extensionUtils = imports.misc.extensionUtils;
-        paperwm = extensionUtils[uuid];
-    } else {
-        extensionSystem = imports.ui.main.extensionManager;
-        extensionUtils = extensionSystem;
-        paperwm = extensionSystem.lookup(uuid);
-    }
-    if (paperwm)
-        throw new Error("paperwm-loaded");
+INSTALL FAILED:
 
-    let dir = Gio.File.new_for_path(path + "/" + uuid);
-    let extension = extensionUtils.createExtensionObject(uuid, dir, 2);
+A previous (non-symlinked) installation of PaperWM already exists at:
+"$EXT".
 
-    extensionSystem.loadExtension(extension);
-    extensionSystem.enableExtension(uuid);
-    true
-} catch (e) {
-    if (e.message === "paperwm-loaded")
-	true
-    else
-        e.message + "  " + e.stack;
-};
+Please remove the installed version from that path and re-run this install script.
+
 EOF
-`
-echo "Trying to load and enable extension:"
-RET=`gdbus call --session -d org.gnome.Shell -o /org/gnome/Shell -m org.gnome.Shell.Eval "$ENABLE"`
-if [[ "(true, 'true')" == "$RET" ]]; then
-    # Enable with gnome-extensions tool since `extensionSystem.enableExtension`
-    # doesn't write to dconf in version < 3.34
-    if type gnome-extensions &> /dev/null; then
-        gnome-extensions enable "$UUID"
-    else
-        gnome-shell-extension-tool --enable="$UUID"
-    fi
-else
-    # String is already quoted and escaped; we want to print the unescaped version
-    unescaped="$(echo "$RET" | sed -e "s/(true, '\"//" | sed -e "s/\\\\n/\n/g")"
-
-    echo "Could not enable PaperWM automatically. You may need to enable it manually."
-    printf 'Debug output: %s\n' "$unescaped"
+exit 1
 fi
+
+ln -snf "$REPO" "$EXT"
+
+cat << EOF
+
+INSTALL SUCCESSFUL:
+
+If this is the first time installing PaperWM, then please logout/login 
+and enable the PaperWM extension, either with the Gnome Extensions application, 
+or manually by executing the following command from a terminal:
+
+/usr/bin/gnome-extensions enable ${UUID}
+
+EOF
