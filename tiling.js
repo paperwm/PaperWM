c90de0d0ccffcb74da0fd7bb2f92241cf6507932
Matthias Seiffert <matthias-seiffert@hotmail.de>
2023-10-27 19:06:10 +0200

    Strict monitor comparison

--- tiling.js
+++ tiling.js
@@ -2170,7 +2170,7 @@ var Spaces = class Spaces extends Map {
         let out = [];
         for (let i = 0; i < nWorkspaces; i++) {
             let space = this.spaceOf(workspaceManager.get_workspace_by_index(i));
-            if (this.monitors.get(space.monitor) !== space || space.monitor == monitor) {
+            if (this.monitors.get(space.monitor) !== space || space.monitor === monitor) {
                 // include workspace if it is the current one
                 // or if it is not active on another monitor
                 out.push(space);
