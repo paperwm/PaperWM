5ea03f52a82b22bc23ef136b7b747e8001ee73d7
Jay Ta'ala <jay@jaytaala.com>
2023-11-05 23:00:26 +1100

    FIX: wasn't enough to ensureViewport (on scratch window) - needed to call `moveDone` - now passing through ensureViewport.

--- tiling.js
+++ tiling.js
@@ -234,7 +234,9 @@ var Space = class Space extends Array {
             // warp pointer to monitor that is active
             Utils.warpPointerToMonitor(this.monitor);
             Utils.later_add(Meta.LaterType.IDLE, () => {
-                ensureViewport(display.focus_window, this, { moveto: true, force: true });
+                this.moveDone(() => {
+                    ensureViewport(display.focus_window, this, { moveto: true, force: true });
+                });
             });
         });
 
