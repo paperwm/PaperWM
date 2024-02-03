1dc3489e88ec225ba437ad8cbf9197e2b2fe01f0
Matthias Seiffert <matthias-seiffert@hotmail.de>
2024-02-03 15:03:19 +0100

    Rename script and add usage

--- dbus-monitor.sh
+++ dbus-monitor.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
 #
 # Usage:
-# monitor.sh
-# monitor.sh --profile  # table like tab separated output
-# monitor.sh --signals  # only show signals
-# monitor.sh FILTER  # see https://dbus.freedesktop.org/doc/dbus-specification.html#message-bus-routing-match-rules
-# monitor.sh type=signal
+# dbus-monitor.sh
+# dbus-monitor.sh --profile  # table like tab separated output
+# dbus-monitor.sh --signals  # only show signals
+# dbus-monitor.sh FILTER  # see https://dbus.freedesktop.org/doc/dbus-specification.html#message-bus-routing-match-rules
+# dbus-monitor.sh type=signal
 
 set -euo pipefail
 
 ADDITIONAL_ARGS=()
 FILTER=("path=/org/github/PaperWM")
+
+script_name=$(basename "${BASH_SOURCE[0]}")
+
+usage() {
+    cat <<EOF
+Usage: ${script_name} [-h|--help] [--profile] [--signals] [FILTER]
+
+Monitor dbus messages to/from PaperWM.
+
+Available options:
+
+  --profile   Output table like, tab seperated data
+  --signals   Only show signals
+  FITLER      Additional filter to be passed to dbus-monitor, multiple can be provided.
+
+Any other options (prefixed with --) are passed as is to dbus-monitor.
+
+EOF
+}
 
 main() {
     while [[ $# -gt 0 ]]; do
@@ -22,6 +41,10 @@ main() {
             --signals)
                 FILTER+=( "type=signal" )
                 shift
+                ;;
+            -h|--help)
+                usage
+                exit
                 ;;
             --*)
                 ADDITIONAL_ARGS+=( "$1" )
